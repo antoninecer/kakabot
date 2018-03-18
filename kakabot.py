de09799c9773b921f24d3c42d7015ac005d77d3d
antoninecer <37349272+antoninecer@users.noreply.github.com>
2018-03-18 15:16:02 +0100

    se zapisem do db
    nezda se, ze by to fungovalo jak ma, musime kouknout na podminky

--- kakabot.py
+++ kakabot.py
@@ -1,4 +1,9 @@
-import yobit_api, json, time, requests, datetime, sys
+import yobit_api, json, time, requests, datetime, mysql.connector,socket, sys
+
+db = "kakabot"
+uzivatel = "kakabot"
+heslo = "kvtd2qBd8W1Qcols"
+server = "127.0.0.1"
 
 maincurr = 'usd'  # usd
 currency = 'eth'  # doge
@@ -90,6 +95,33 @@ def zapis(*s):
     stav = "nic"
     plus = 0
 
+def sql_obchod(kolik):
+    global ccactual, stav, maincurr, currency
+    cnx = mysql.connector.connect(user=uzivatel, password=heslo, host=server, database=db)
+    x = cnx.cursor()
+    veta = "insert into kakabot.kakabot(cas, maincurr, currency, stav, kurz, kolik, kdo) values('"
+    veta += str(datetime.datetime.now()) + "', '" + maincurr + "', '" + currency + "', '" + stav + "',"
+    veta += str(ccactual) + "," + str(kolik) + ",'" + socket.gethostname()  + "')"
+    print(veta)
+    try:
+        x.execute(veta)
+        cnx.commit()
+    except:
+        cnx.rollback()
+
+
+def sql_posledni(s):
+    jmeno = socket.gethostname()
+    cnx = mysql.connector.connect(user=uzivatel, password=heslo, host=server, database=db)
+    x = cnx.cursor()
+    try:
+        x.execute("select kurz from kakabot.kakabot where stav= '" + s +"' and kdo = '" + jmeno + "' order by id desc limit 1")
+        return x.fetchone()[0]
+    except:
+        return 0
+    cnx.close()
+
+
 def sestav_vetu():
     global ccpoint, ccactual, cclast, stav, plus
     text = str(datetime.datetime.now()) + " : " + par + " Point: " + str(ccpoint) + " last: " + str(cclast) + " actual: " + str(ccactual) + " Stav >" + stav + "< plus:>" + str(plus) + "<\n"
@@ -101,6 +133,11 @@ account = dotaz.get('return')
 wallet = account.get('funds')
 
 actvol()
+nakuppri=sql_posledni("nakup")
+prodejpri=sql_posledni("prodej")
+
+print("posledni nakup>" + str(nakuppri))
+print("posledni prodej>" + str(prodejpri))
 
 vloz = vklad
 nacti()
@@ -115,6 +152,7 @@ while run:
         continue
 
     sys.stdout.write('.')
+    sys.stdout.flush()
     veta = str(datetime.datetime.now()) + " : " + par + " : act >" + str(ccactual) + "<: "
 
 
@@ -154,37 +192,44 @@ while run:
     if stav == "prodej" and ccactual < cclast :
         penezenka()
         actvol()
+        print("naposledy nakoupeno za >" + str(nakuppri))
         # for poptavka in aktivni_obchody(pair,"sell"):
          #   print("poptavka[0]> " + str(poptavka[0]) + " poptavka[1]> " + str(poptavka[1]))
         if currencyvol > 0.0:
-            zapis("uskutecni prodej >")
-            print(currency + " >> " + str(currencyvol) + " : " + maincurr + " >> " + str(maincurrvol) + " .")
-            print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).sell(par, ccactual, currencyvol * vloz))
-            zapis("prodej " + str(ccactual) + " za " + str(currencyvol * vloz))
-            prodejpri = ccactual
+            # if nakuppri == 0 or ccactual > nakuppri:
+                zapis("uskutecni prodej >")
+                print(currency + " >> " + str(currencyvol) + " : " + maincurr + " >> " + str(maincurrvol) + " .")
+                print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).sell(par, ccactual, currencyvol * vloz))
+                zapis("prodej " + str(ccactual) + " za " + str(currencyvol * vloz))
+                sql_obchod(str(currencyvol * vloz))
+                prodejpri = ccactual
 
     if stav == "nakup" and ccactual > cclast :
         penezenka()
         actvol()
+        print("naposledy prodano za >" + str(prodejpri))
         # for nabidka in aktivni_obchody(pair,"buy"):
          #   print(" nabidka[0]> " + str(nabidka[0]) + " nabidka[1] > " + str(nabidka[1]))
         if maincurrvol > 0.0:
-            zapis("uskutecni nakup >")  # , str(aktivni_obchody(pair,"buy")))
-            print(currency + " >> " + str(currencyvol) + " : " + maincurr + " >> " + str(maincurrvol) + " .")
-            nakup = (maincurrvol * vloz)/ccactual)
-            print("budem nakupovat za >" + str(nakup))
-            print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).buy(par, ccactual, nakup)
-            zapis("nakup "+str(ccactual)+" za "+str(maincurrvol * vloz))
-            nakuppri = ccactual
+            # if prodejpri == 0 or ccactual < prodejpri:
+                zapis("uskutecni nakup >")  # , str(aktivni_obchody(pair,"buy")))
+                print(currency + " >> " + str(currencyvol) + " : " + maincurr + " >> " + str(maincurrvol) + " .")
+                nakup = (maincurrvol * vloz)/ccactual
+                print("budem nakupovat za >" + str((maincurrvol * vloz)))
+                print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).buy(par, ccactual, nakup))
+                zapis("nakup "+str(ccactual)+" za "+str(maincurrvol * vloz))
+                sql_obchod(str(maincurrvol * vloz))
+                nakuppri = ccactual
 
     if ccactual != cclast:
         print(ccactual)
     cclast=ccactual
 
     time.sleep(10)
-
+# sql_obchod(0.0123)
 # prodam print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).buy("eth_usd", 634.365, 0.015))
 
 #print(yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).get_active_orders("eth_usd"))
 #yobit_api.TradeApi(key=yobit_key, secret_key=yobit_secret_key).buy(pair, ccactual, maincurrvol )
 # 17.3 12:44 eth >> 0.015 : usd >> 11.52376607
+# 13>58  eth >> 0.02003553 : usd >> 6.00754875
